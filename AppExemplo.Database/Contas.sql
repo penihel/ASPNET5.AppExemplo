903b8aa22e7a0dc393e33199db1aacaebde8a4af
Penihel Roosewelt <penihel@gmail.com>
2015-06-30 23:44:53 -0300

    Estrutura inicial do banco de dados

---
+++ AppExemplo.Database/Contas.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Contas]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [ClienteId] UNIQUEIDENTIFIER NOT NULL, 
+    [DataCriacao] DATETIME NOT NULL DEFAULT GETDATE(), 
+    [Numero] NVARCHAR(50) NOT NULL, 
+    [Ativo] BIT NOT NULL, 
+    CONSTRAINT [FK_Contas_Clientes] FOREIGN KEY ([ClienteId]) REFERENCES [Clientes]([Id]), 
+    CONSTRAINT [UK_Contas_Numero] UNIQUE ([Numero]) 
+)
